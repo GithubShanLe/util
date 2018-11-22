c6d32f24e3c42472052fdb34e65f56762262f508
root <root@localhost.localdomain>
2018-11-22 00:25:29 -0500

    1

--- kill.sh
+++ kill.sh
@@ -1,9 +1,1 @@
-process=`ps -ef|grep $1 |grep -v grep`;
-if [ "$process" == "" ];then
-	echo "NO";
-	break;
-else 
-	echo "Yes";
-    ps -ef|grep $1 |grep -v grep;
-	ps -ef|grep $1 |grep -v grep|cut -c 9-15|xargs kill ;
-fi
+ps -ef|grep $1 |grep -v grep|cut -c 9-15|xargs kill -9
